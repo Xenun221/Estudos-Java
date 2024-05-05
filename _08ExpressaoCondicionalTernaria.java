20db9236296ea259e1984ee2521ad37e134f1e11
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-05-05 12:18:49 -0300

    Aula 08 Expressão Condição Ternaria
    Aula de expressão Ternaria

---
+++ _08ExpressaoCondicionalTernaria.java
@@ -0,0 +1,21 @@
+public class _08ExpressaoCondicionalTernaria {
+    public static void main(String[] args) {
+        /*
+        double preco = 34.5;
+        double desconto;
+        if (preco < 20.0){
+            desconto = preco * 0.1;
+        }
+        else {
+            desconto = preco * 0.5;
+        }
+        DEMONSTRAÇÃO SEM OPÇÃO TERNARIA
+         */
+
+        //Condição Ternaria
+        double preco = 34.5;
+        double desconto = (preco < 20.0) ? preco * 0.1: preco * 0.05;
+        System.out.println(desconto);
+
+    }
+}
